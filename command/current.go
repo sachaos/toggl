6595e3a45347d8161d12f4cbe16eec9bfb661ffa
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2016-12-18 23:36:27 +0900

    :sparkles: Add duration view on current command

--- command/current.go
+++ command/current.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 	"os"
 	"text/tabwriter"
+	"time"
 
 	"github.com/sachaos/toggl/lib"
 	"github.com/spf13/viper"
 	"github.com/urfave/cli"
 )
+
+func formatTimeDuration(duration time.Duration) string {
+	hours := duration / time.Hour
+	minutes := duration / time.Minute % 60
+	seconds := duration / time.Second % 60
+	return fmt.Sprintf("%d:%02d:%02d", hours, minutes, seconds)
+}
+
+func calcDuration(duration int64) time.Duration {
+	second := duration + time.Now().Unix()
+	return time.Duration(second * int64(time.Second))
+}
 
 func CmdCurrent(c *cli.Context) error {
 
@@ -28,6 +41,7 @@ func CmdCurrent(c *cli.Context) error {
 
 	fmt.Fprintf(w, "ID\t%d\n", current_time_entry.ID)
 	fmt.Fprintf(w, "Description\t%s\n", current_time_entry.Description)
+	fmt.Fprintf(w, "Duration\t%s\n", formatTimeDuration(calcDuration(current_time_entry.Duration)))
 	w.Flush()
 
 	return nil
